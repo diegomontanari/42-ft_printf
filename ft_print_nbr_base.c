8cc51f11a7da77202d19b8cb76bcffda753c1c16
diegomontanari <diegomontanari.imp@gmail.com>
2025-02-28 15:56:52 +0100

    Update ft_print_nbr_base.c

--- ft_print_nbr_base.c
+++ ft_print_nbr_base.c
@@ -16,15 +16,15 @@ void	ft_print_nbr_base(unsigned long nbr, char *base, int *count)
 {
 	size_t	len;
 
-	len = ft_strlen(base);
+	len = ft_strlen(base); // Compute the length of the base using the ft_strlen function
 	if (nbr == 0)
 	{
-		ft_print_char(base[0], count);
+		ft_print_char(base[0], count); // If the number is 0, print the first character of the base (1st is 0 for every base) and increment the count
 		return ;
 	}
 	if (nbr >= len)
 	{
-		ft_print_nbr_base(nbr / len, base, count);
+		ft_print_nbr_base(nbr / len, base, count); // recursively call the function with the number divided by the length of the base
 	}
-	ft_print_char(base[nbr % len], count);
+	ft_print_char(base[nbr % len], count); // Print the character in the base that corresponds to the remainder of the number divided by the length of the base and increment the count
 }
